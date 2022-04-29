cd5ca6dab4861bd15a5954e3a0b9e8fe1a79daa2
mapsteps <connect@mapsteps.com>
2022-04-29 19:11:22 +0200

    sidebar complete

--- templates/setting-boxes/recommended.php
+++ templates/setting-boxes/recommended.php
@@ -10,18 +10,22 @@ defined( 'ABSPATH' ) || die( "Can't access directly" );
 
 <div class="heatbox">
 	<h2>
-		<?php _e( 'Wants more options?', 'erident-custom-login-and-dashboard' ); ?>
+		<?php _e( 'More Options available with Ultimate Dashboard!', 'erident-custom-login-and-dashboard' ); ?>
 	</h2>
 	<div class="heatbox-content">
-		<p class="description">
-			Wants to see your login page results instantly while customizing it?
+		<p>
+			<a href="https://wordpress.org/plugins/ultimate-dashboard/" target="_blank">
+				<img style="max-width:100%;" src="<?php echo esc_url( CUSTOM_LOGIN_DASHBOARD_PLUGIN_URL ); ?>/assets/images/ultimate-dashboard-sidebar.jpg">
+			</a>
 		</p>
 		<p class="description">
-			<a href="https://wordpress.org/plugins/ultimate-dashboard/" target="_blank">Ultimate Dashboard</a> provides a way to fully customize your WordPress admin area. 
-			It has a lot of features. David will explain it.
+			<?php _e( 'Customize your WordPress login screen in real-time right within the WordPress customizer. But there is more!', 'erident-custom-login-and-dashboard' ); ?>
 		</p>
-		<a href="https://ultimatedashboard.io/" target="_blank" class="button button-primary">
-			<?php _e( 'Check Ultimate Dashboard', 'erident-custom-login-and-dashboard' ); ?>
+		<p class="description">
+			<a href="https://wordpress.org/plugins/ultimate-dashboard/" target="_blank">Ultimate Dashboard</a> allows you to <strong>fully customize the WordPress admin</strong> area.
+		</p>
+		<a href="https://wordpress.org/plugins/ultimate-dashboard/" target="_blank" class="button button-primary button-larger button-full">
+			<?php _e( 'Check out Ultimate Dashboard', 'erident-custom-login-and-dashboard' ); ?>
 		</a>
 	</div>
 </div>
--- templates/setting-boxes/review.php
+++ templates/setting-boxes/review.php
@@ -13,8 +13,11 @@ defined( 'ABSPATH' ) || die( "Can't access directly" );
 		<?php _e( 'Leave a Review', 'erident-custom-login-and-dashboard' ); ?>
 	</h2>
 	<div class="heatbox-content">
+		<p class="review-icons">
+			<span class="dashicons dashicons-star-filled"></span><span class="dashicons dashicons-star-filled"></span><span class="dashicons dashicons-star-filled"></span><span class="dashicons dashicons-star-filled"></span><span class="dashicons dashicons-star-filled"></span>
+		</p>
 		<p class="description">
-			<?php _e( 'Do you enjoy Erident Custom Login and Dashboard? Help us spread the word and leave a review in the official WordPress repository.', 'erident-custom-login-and-dashboard' ); ?>
+			<?php _e( 'Do you enjoy <strong>Erident Custom Login and Dashboard</strong>? Help us spread the word and leave a review in the official WordPress repository.', 'erident-custom-login-and-dashboard' ); ?>
 		</p>
 		<a href="https://wordpress.org/plugins/erident-custom-login-and-dashboard/reviews/#new-post" target="_blank" class="button">
 			<?php _e( 'Sure, you deserve it!', 'erident-custom-login-and-dashboard' ); ?>
